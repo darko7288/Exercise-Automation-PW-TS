eabc772b64f5f58e30794724e0472054e8c9c585
Darko Puhalo <darko7288@gmail.com>
2023-12-23 18:42:11 +0100

    Implement POM for the Access Page.

---
+++ UI/Constants.ts
@@ -0,0 +1,36 @@
+/*************************************************/
+/*                 Credentials                   */
+/*********************************************** */
+
+export const name = "darko7288";
+
+export const email = "darko7288@gmail.com";
+export const incorrectEmail = "d8@gmail.com";
+export const invalidEmail = "darko7288gmail.com";
+
+export const password = "7288";
+export const incorrectPassword = "728";
+
+/*************************************************/
+/*            Page Titles and URLs               */
+/*********************************************** */
+
+export const accessPageTitle = "Automation Exercise - Signup / Login";
+export const accessPageURL = "https://automationexercise.com/login";
+
+/*************************************************/
+/*                 Access Page                   */
+/*********************************************** */
+
+export const loginBannerText = "Login to your account";
+export const loginEmailInputPlaceholder = "Email Address";
+export const loginPasswordInputPlaceholder = "Password";
+export const loginButtonText = "Login";
+
+export const signupBannerText = "New User Signup!";
+export const signupNameInputPlaceholder = "Name";
+export const signupEmailInputPlaceholder = "Email Address";
+export const signupButtonText = "Signup";
+
+export const separatorText = "OR";
+
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,18 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+	timeout: 60000,
+	retries: 0,
+
 	use: 
 	{
+		testIdAttribute: "data-qa",
 		headless: true,
 		viewport: { width: 1280, height: 720 },
+		actionTimeout: 10000,
 		ignoreHTTPSErrors: true,
+		screenshot: "off",
+		video: "off"
 	},
 
 	testMatch: /Tests_UI.*.spec.ts/,
